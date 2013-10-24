78ef83ed38f0de989db57d0df419774a8aff9602
Torsten Trautwein <trautwein@neowork.com>
2013-10-24 16:26:05 +0200

    oh-my-zsh

---
+++ symlink.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+############################
+# This script creates symlinks from the home directory to any desired dotfiles in ~/dotfiles
+############################
+
+########## Variables
+
+dir=~/dotfiles                    # dotfiles directory
+olddir=~/dotfiles_old             # old dotfiles backup directory
+
+##########
+
+# create dotfiles_old in homedir
+echo -n "Creating $olddir for backup of any existing dotfiles in ~ ..."
+mkdir -p $olddir
+echo "done"
+
+# change to the dotfiles directory
+echo -n "Changing to the $dir directory ..."
+cd $dir
+echo "done"
+
+# move any existing dotfiles in homedir to dotfiles_old directory, then create symlinks from the homedir to any files in the ~/dotfiles directory specified in $files
+echo "Moving any existing dotfiles from ~ to $olddir and creating symlinks to the dotfiles."
+for file in $dir/*; do
+  filename=$(basename "$file")
+  if [ "$filename" != "symlink.sh" ]; then
+    echo $filename
+    cp ~/.$filename ~/dotfiles_old/$filename
+    rm ~/.$filename
+
+    ln -s $file ~/.$filename
+  fi
+done
+
+function clone_oh_my_zsh {
+  # Test to see if zshell is installed.  If it is:
+  if [ -f /bin/zsh -o -f /usr/bin/zsh ]; then
+    # Clone oh-my-zsh repository from GitHub only if it isn't already present
+    if [ ! -d ~/.oh-my-zsh/ ]; then
+      git clone http://github.com/neowork/oh-my-zsh.git ~/.oh-my-zsh
+    fi
+
+    # Set the default shell to zsh if it isn't currently set to zsh
+    if [ ! $(echo $SHELL) == $(which zsh) ]; then
+      chsh -s $(which zsh)
+    fi
+  else
+    # If the platform is OS X, tell the user to install zsh
+    echo "Please install zsh, then re-run this script!"
+    exit
+  fi
+}
+
+clone_oh_my_zsh
---
+++ zshrc
@@ -0,0 +1,53 @@
+# Path to your oh-my-zsh configuration.
+ZSH=$HOME/.oh-my-zsh
+
+# Set name of the theme to load.
+# Look in ~/.oh-my-zsh/themes/
+# Optionally, if you set this to "random", it'll load a random theme each
+# time that oh-my-zsh is loaded.
+ZSH_THEME="miloshadzic"
+
+# Example aliases
+# alias zshconfig="mate ~/.zshrc"
+# alias ohmyzsh="mate ~/.oh-my-zsh"
+
+# Set to this to use case-sensitive completion
+# CASE_SENSITIVE="true"
+
+# Comment this out to disable bi-weekly auto-update checks
+# DISABLE_AUTO_UPDATE="true"
+
+# Uncomment to change how many often would you like to wait before auto-updates occur? (in days)
+# export UPDATE_ZSH_DAYS=13
+
+# Uncomment following line if you want to disable colors in ls
+# DISABLE_LS_COLORS="true"
+
+# Uncomment following line if you want to disable autosetting terminal title.
+# DISABLE_AUTO_TITLE="true"
+
+# Uncomment following line if you want red dots to be displayed while waiting for completion
+# COMPLETION_WAITING_DOTS="true"
+
+export LC_ALL=en_US.UTF-8
+export LC_CTYPE=en_US.UTF-8
+export LANG=en_US.UTF-8
+export CC=/usr/local/bin/gcc-4.2
+export PATH=/usr/local/sbin:/usr/local/bin:$PATH
+export PATH="/usr/local/heroku/bin:$PATH"
+
+# Which plugins would you like to load? (plugins can be found in ~/.oh-my-zsh/plugins/*)
+# Custom plugins may be added to ~/.oh-my-zsh/custom/plugins/
+# Example format: plugins=(rails git textmate ruby lighthouse)
+plugins=(git brew bundler osx rails3 rbenv)
+
+source $ZSH/oh-my-zsh.sh
+unsetopt correct_all
+
+# Customize to your needs...
+if which rbenv > /dev/null; then eval "$(rbenv init -)"; fi
+
+# ENV
+if [ -f ~/.zshrc.env ]; then
+  source ~/.zshrc.env
+fi
